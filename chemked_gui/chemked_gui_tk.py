74e98d9786f1aade520022b7e62032a5bf88e98b
Michael Bernard <mike.bernard0423@gmail.com>
2018-12-09 01:40:15 -0600

    add skeleton of mutable data
    User will modify self.data with the GUI. Upon exporting, data in self.data is reformatted from tkinter data types into regular Python data types and passed to ChemKED for export.

--- chemked_gui/chemked_gui_tk.py
+++ chemked_gui/chemked_gui_tk.py
@@ -2,6 +2,7 @@
 # import sys
 from tkinter import *
 # import tkinter.messagebox
+from pyked import __version__, chemked
 
 
 class MainWindow(Frame):
@@ -10,12 +11,58 @@ class MainWindow(Frame):
     """
 
     def __init__(self, parent=None):
+        # set up the window
         Frame.__init__(self, parent, width=800, height=600)
         self.parent = parent
         self.pack()
         self.winfo_toplevel().title("ChemKED")
         self.center_window()
         self.create_menu_bar()
+
+        # the data the user will modify and export with the GUI
+        self.data = {
+            "file-authors": [
+                {"name": Entry(self),
+                 "ORCID": Entry(self)}
+            ],
+            "file-version": Entry(self),
+            "chemked-version": Entry(self),
+            "reference": {
+                "doi": Entry(self),
+                "authors": [
+                    {"name": Entry(self),
+                     "ORCID": Entry(self)}
+                ],
+                "journal": Entry(self),
+                "year": Entry(self),
+                "volume": Entry(self),
+                "pages": Entry(self),
+                "detail": Entry(self)
+            },
+            "experiment-type": '',  #TODO this should be a combo box
+            "apparatus": {
+                "kind": '',  #TODO this should be a combo box
+                "institution": Entry(self),
+                "facility": Entry(self)
+            },
+            "datapoints": [
+                {"temperature": Entry(self),
+                 "pressure": Entry(self),
+                 "ignition-delay": Entry(self),
+                 "equivalence-ratio": Entry(self)}
+            ],
+            "common-properties": {
+                "species": [
+                    {"species-name": Entry(self),
+                     "InChI": Entry(self),
+                     "amount": Entry(self)}
+                ],
+                "kind": '',  #TODO this should be a combo box
+                "ignition-target": '', #TODO this should be a combo box
+                "ignition-type": '', #TODO this should be a combo box
+            }
+        }
+        self.set_init_data()
 
     def center_window(self):
         """ Centers the window on the user's screen. """
@@ -33,15 +80,20 @@ class MainWindow(Frame):
         self.winfo_toplevel().config(menu=menu)
 
         file_menu = Menu(menu)
-        file_menu.add_command(label="Preferences", command=self.preferences)
+        file_menu.add_command(label="TODO: Preferences", command=self.preferences)
+        file_menu.add_separator()
         file_menu.add_command(label="Exit", command=self.winfo_toplevel().quit)
         menu.add_cascade(label="File", menu=file_menu)
 
         edit_menu = Menu(menu)
+        edit_menu.add_command(label="TODO: Put something here")
         menu.add_cascade(label="Edit", menu=edit_menu)
 
     def preferences(self):
         print("This feature will be added in a future update!")
+
+    def set_init_data(self):
+        self.data["chemked-version"].insert(END, __version__)
 
     def create_status_bar(self):
         pass
