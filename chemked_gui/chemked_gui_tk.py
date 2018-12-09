7f1acc59b304cc5c9516773cdc4e3057deb85b47
Michael Bernard <mike.bernard0423@gmail.com>
2018-12-09 01:25:30 -0600

    add method skeleton and menu bar

--- chemked_gui/chemked_gui_tk.py
+++ chemked_gui/chemked_gui_tk.py
@@ -1,7 +1,7 @@
-import os
-import sys
+# import os
+# import sys
 from tkinter import *
-import tkinter.messagebox
+# import tkinter.messagebox
 
 
 class MainWindow(Frame):
@@ -15,6 +15,7 @@ class MainWindow(Frame):
         self.pack()
         self.winfo_toplevel().title("ChemKED")
         self.center_window()
+        self.create_menu_bar()
 
     def center_window(self):
         """ Centers the window on the user's screen. """
@@ -23,10 +24,36 @@ class MainWindow(Frame):
         screen_height = self.winfo_toplevel().winfo_screenheight()
         window_width = self.winfo_toplevel().winfo_width()
         window_height = self.winfo_toplevel().winfo_height()
-        print(screen_width, screen_height, window_width, window_height)
         x = (screen_width // 2) - (window_width // 2)
         y = (screen_height // 2) - (window_height // 2)
         self.winfo_toplevel().geometry("{}x{}+{}+{}".format(window_width, window_height, x, y))
+
+    def create_menu_bar(self):
+        menu = Menu(self)
+        self.winfo_toplevel().config(menu=menu)
+
+        file_menu = Menu(menu)
+        file_menu.add_command(label="Preferences", command=self.preferences)
+        file_menu.add_command(label="Exit", command=self.winfo_toplevel().quit)
+        menu.add_cascade(label="File", menu=file_menu)
+
+        edit_menu = Menu(menu)
+        menu.add_cascade(label="Edit", menu=edit_menu)
+
+    def preferences(self):
+        print("This feature will be added in a future update!")
+
+    def create_status_bar(self):
+        pass
+
+    def create_tabs(self):
+        pass
+
+    def verify_inputs(self):
+        pass
+
+    def export_to_yaml(self):
+        pass
 
 
 def main():
